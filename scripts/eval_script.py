56f52374d6b45cdf6430809c0bad43db51f1044f
Shreeya Khurana <khurana.shreeya@gmail.com>
2024-11-21 11:14:00 -0500

    fixed evaluation script

--- scripts/eval_script.py
+++ scripts/eval_script.py
@@ -25,11 +25,16 @@ def visualize_masks(seg_mask, gt_mask):
 
 def dice_score(mask1, mask2):
     intersection = np.logical_and(mask1, mask2)
+    # if union is 0, return 1
+    if mask1.sum() + mask2.sum() == 0:
+        return 1.0
     return 2. * intersection.sum() / (mask1.sum() + mask2.sum())
 
 def iou(mask1, mask2):
     intersection = np.logical_and(mask1, mask2)
     union = np.logical_or(mask1, mask2)
+    if union.sum() == 0:
+        return 1.0
     return intersection.sum() / union.sum()
 
 def preproces_gt(gt_mask):
@@ -38,27 +43,38 @@ def preproces_gt(gt_mask):
     gt_mask_resized = gt_mask
     return gt_mask_resized
 
+def preprocess_seg(seg_mask):
+    seg_mask[seg_mask > 0.8] = 1
+    seg_mask[seg_mask < 0.4] = 0
+    #seg_mask_resized = seg_mask[..., 8:-8, 8:-8]
+    seg_mask_resized = seg_mask
+    return seg_mask_resized
 
 
-seg_pred_dir = '../midpoint_eval_results/large_model_generations/results_large_model_midpoint_eval_samples'
-ground_truth_dir = '../midpoint_eval_results/midpoint_eval_samples'
+
+seg_pred_dir = '../full_training_eval_results/BraTS20_Training/'
+ground_truth_dir = '../full_training_eval_results/test_slices/'
 
 dice_vals = []
 iou_vals = []
 
 for t in os.listdir(ground_truth_dir):
-    base_name = t.split('_slice')[0]
-    gt_mask = nib.load(os.path.join(ground_truth_dir, t, base_name+"_seg.nii.gz")).get_fdata()
+    base_name = t.split('_slice')[0] #BraTS20_Training_007
+    print(f"Processing {t}")
+    gt_mask = nib.load(os.path.join(ground_truth_dir, t, base_name+"_seg.nii")).get_fdata()
     gt_mask = preproces_gt(gt_mask)
+    
     seg_mask = torch.load(os.path.join(seg_pred_dir, t+"_output0"), map_location=torch.device('cpu'))
     seg_mask = seg_mask.numpy()
     seg_mask = np.squeeze(seg_mask)
+    seg_mask = preprocess_seg(seg_mask)
 
     dice_val = dice_score(seg_mask, gt_mask)
     iou_val = iou(seg_mask, gt_mask)
     dice_vals.append(dice_val)
     iou_vals.append(iou_val)
-    visualize_masks(seg_mask, gt_mask)
+    #visualize_masks(seg_mask, gt_mask)
+    #breakpoint()
 
 
 avg_dice = np.mean(dice_vals)
