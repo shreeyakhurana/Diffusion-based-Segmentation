e6778e2904e0f5761d61dc47fc2d1853b30d2e0d
Shreeya Khurana <khurana.shreeya@gmail.com>
2024-12-05 22:49:35 -0500

    cleaning up eval and viz code

--- results/visualize_images.py
+++ results/visualize_images.py
@@ -8,51 +8,29 @@ import nibabel as nib
 import os
 import numpy as np
 
-tensor = torch.load("eval_on_train_samples/000001_output0", map_location=torch.device('cpu'))
-print(type(tensor), tensor.shape)
-print(tensor[0][0][0])
 
-test_sample1_paths = [
-    '../data/testing/000246/brats_train_006_flair_080_w.nii',
-    '../data/testing/000246/brats_train_006_t1_080_w.nii',
-    '../data/testing/000246/brats_train_006_t1ce_080_w.nii',
-    '../data/testing/000246/brats_train_006_t2_080_w.nii'
-]
 train_sample1_paths = [
-    '../midpoint_eval_results/midpoint_eval_samples/BraTS20_Training_010_slice_2/BraTS20_Training_010_flair.nii.gz',
-    '../midpoint_eval_results/midpoint_eval_samples/BraTS20_Training_010_slice_2/BraTS20_Training_010_seg.nii.gz',
-    '../midpoint_eval_results/midpoint_eval_samples/BraTS20_Training_010_slice_2/BraTS20_Training_010_t1.nii.gz',
-    '../midpoint_eval_results/midpoint_eval_samples/BraTS20_Training_010_slice_2/BraTS20_Training_010_t1ce.nii.gz',
-    '../midpoint_eval_results/midpoint_eval_samples/BraTS20_Training_010_slice_2/BraTS20_Training_010_t2.nii.gz',
+    'full_training_eval_results/test_slices/BraTS20_Training_007_slice_23/BraTS20_Training_007_flair.nii',
+    'full_training_eval_results/test_slices/BraTS20_Training_007_slice_23/BraTS20_Training_007_seg.nii',
+    'full_training_eval_results/test_slices/BraTS20_Training_007_slice_23/BraTS20_Training_007_t1.nii',
+    'full_training_eval_results/test_slices/BraTS20_Training_007_slice_23/BraTS20_Training_007_t1ce.nii',
+    'full_training_eval_results/test_slices/BraTS20_Training_007_slice_23/BraTS20_Training_007_t2.nii',
 ]
-train_sample2_paths = [
-    '../data/training/000002/brats_train_001_flair_081_w.nii.gz',
-    '../data/training/000002/brats_train_001_t1_081_w.nii.gz',
-    '../data/training/000002/brats_train_001_t1ce_081_w.nii.gz',
-    '../data/training/000002/brats_train_001_t2_081_w.nii.gz',
-    '../data/training/000002/brats_train_001_seg_081_w.nii.gz',
-]
-img = nib.load(train_sample1_paths[3]).get_fdata()
-print(img.shape)
-print(f"The .nii files are stored in memory as numpy's: {type(img)}.")
 
 plt.style.use('default')
 fig, axes = plt.subplots(1, 6, figsize=(15,3))
-train_samples = [train_sample2_paths]
+train_samples = [train_sample1_paths]
 for i, ax in enumerate(axes.reshape(-1)):
     j = i // 6
     k = i % 6
     if k == 5:
-        seg_mask = torch.load(os.path.join('../midpoint_eval_results/large_model_generations/results_large_model_midpoint_eval_samples/BraTS20_Training_010_slice_2_output0'), map_location=torch.device('cpu'))
+        seg_mask = torch.load(os.path.join('full_training_eval_results/ddim_smallset_750steps/BraTS20_Training_007_slice_23_output0.zip'), map_location=torch.device('cpu'))
         seg_mask = seg_mask.numpy()
         seg_mask = np.squeeze(seg_mask)
+        seg_mask = seg_mask[0]
         ax.imshow(seg_mask)
     else:
         img = nib.load(train_samples[j][k]).get_fdata()
         ax.imshow(img)
 
-# seg_mask = torch.load(os.path.join('results/eval_on_train_samples/000001_output0'), map_location=torch.device('cpu'))
-# seg_mask = seg_mask.numpy()
-# seg_mask = np.squeeze(seg_mask)
-# ax.imshow(seg_mask)
 plt.show()
--- scripts/eval_script.py
+++ scripts/eval_script.py
@@ -12,7 +12,7 @@ def visualize_masks(seg_mask, gt_mask):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     
     ax1.imshow(seg_mask, cmap='gray')
-    ax1.set_title('Segmentation Mask')
+    ax1.set_title('Predicted Mask')
     ax1.axis('off')
     
     ax2.imshow(gt_mask, cmap='gray')
@@ -23,12 +23,37 @@ def visualize_masks(seg_mask, gt_mask):
 
     plt.show()
 
+def visualize_masks2(seg_mask1, seg_mask2, seg_mask3):
+    import matplotlib.pyplot as plt
+    
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 5))
+    
+    ax1.imshow(seg_mask1, cmap='gray')
+    ax1.set_title('Predicted Mask - 500 steps')
+    ax1.axis('off')
+    
+    ax2.imshow(seg_mask2, cmap='gray')
+    ax2.set_title('Predicted Mask - 750 steps')
+    ax2.axis('off')
+
+    ax3.imshow(seg_mask3, cmap='gray')
+    ax3.set_title('Predicted Mask - 1000 steps')
+    ax3.axis('off')
+    
+    plt.tight_layout()
+
+    plt.show()
+
 def dice_score(mask1, mask2):
     intersection = np.logical_and(mask1, mask2)
     # if union is 0, return 1
-    if mask1.sum() + mask2.sum() == 0:
-        return 1.0
     return 2. * intersection.sum() / (mask1.sum() + mask2.sum())
+
+def dice_score2(pred, targs):
+    pred = torch.tensor(pred)
+    targs = torch.tensor(targs)
+    pred = (pred>0).float()
+    return 2. * (pred*targs).sum() / (pred+targs).sum()
 
 def iou(mask1, mask2):
     intersection = np.logical_and(mask1, mask2)
@@ -39,20 +64,17 @@ def iou(mask1, mask2):
 
 def preproces_gt(gt_mask):
     gt_mask[gt_mask > 0] = 1
-    #gt_mask_resized = gt_mask[..., 8:-8, 8:-8] 
     gt_mask_resized = gt_mask
     return gt_mask_resized
 
 def preprocess_seg(seg_mask):
-    seg_mask[seg_mask > 0.8] = 1
-    seg_mask[seg_mask < 0.4] = 0
-    #seg_mask_resized = seg_mask[..., 8:-8, 8:-8]
+    seg_mask[seg_mask > 0.5] = 1
+    seg_mask[seg_mask < 0.5] = 0
     seg_mask_resized = seg_mask
     return seg_mask_resized
 
 
-
-seg_pred_dir = '../full_training_eval_results/ddim_results/'
+seg_pred_dir = '../full_training_eval_results/ddim_smallset_500steps_ensemble5/'
 ground_truth_dir = '../full_training_eval_results/test_slices/'
 
 dice_vals = []
@@ -60,27 +82,32 @@ iou_vals = []
 
 for t in os.listdir(ground_truth_dir):
     base_name = t.split('_slice')[0] #BraTS20_Training_007
-    print(f"Processing {t}")
     gt_file_name = os.path.join(ground_truth_dir, t, base_name+"_seg.nii")
-    print(f"Ground truth file: {gt_file_name}")
     gt_mask = nib.load(gt_file_name).get_fdata()
     gt_mask = preproces_gt(gt_mask)
+
+    #if gt is all zeros, skip
+    if gt_mask.sum() == 0:
+        continue
     
-    seg_file_name = os.path.join(seg_pred_dir, t+"_output0")
-    print(f"Segmentation file: {seg_file_name}")
+    seg_file_name = os.path.join(seg_pred_dir, t+"_output0.zip")
+    if not os.path.exists(seg_file_name):
+        continue
+    print(f"Processing {t}")
     seg_mask = torch.load(seg_file_name, map_location=torch.device('cpu'))
     seg_mask = seg_mask.numpy()
     seg_mask = np.squeeze(seg_mask)
     seg_mask = preprocess_seg(seg_mask)
-    seg_mask = seg_mask[0]
+    seg_mask = seg_mask[1]
 
 
-    dice_val = dice_score(seg_mask, gt_mask)
+    dice_val = dice_score2(seg_mask, gt_mask)
+    print(f"Dice score: {dice_val:.4f}")
     iou_val = iou(seg_mask, gt_mask)
     dice_vals.append(dice_val)
     iou_vals.append(iou_val)
-    # visualize_masks(seg_mask, gt_mask)
-    # breakpoint()
+    #visualize_masks(seg_mask, gt_mask)
+    #breakpoint()
 
 
 avg_dice = np.mean(dice_vals)
